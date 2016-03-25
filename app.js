9ec84cff68b93e038125b1a71b32787d5c3f48cb
Jordan Patton <jordanpatton@gmail.com>
2016-03-25 13:29:22 -0700

    Fixed configuration environment variables.

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 var express = require('express');
 
 /* configuration */
-var CONFIGURATION_PORT = parseInt((process.env.PORT || 3000), 10);
-if (isNaN(CONFIGURATION_PORT)) {
-  console.log('invalid port:', CONFIGURATION_PORT);
+var STATIC_SERVER_PATH = process.env.STATIC_SERVER_PATH || __dirname;
+var STATIC_SERVER_PORT = parseInt((process.env.STATIC_SERVER_PORT || 3000), 10);
+if (isNaN(STATIC_SERVER_PORT)) {
+  console.log('invalid port:', STATIC_SERVER_PORT);
   process.exit(1);
 }
 
@@ -12,7 +13,7 @@ if (isNaN(CONFIGURATION_PORT)) {
 var app = express();
 
 /* middleware */
-app.use('/', express.static(__dirname));
+app.use('/', express.static(STATIC_SERVER_PATH));
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Cache-Control', 'no-cache');
@@ -20,10 +21,11 @@ app.use(function(req, res, next) {
 });
 
 /* initialization */
-app.listen(CONFIGURATION_PORT, function() {
+app.listen(STATIC_SERVER_PORT, function() {
   console.log(
-    'development http server running at' +
-    '\n' + ' => http://localhost:' + CONFIGURATION_PORT +
+    'static-server serving files' +
+    '\n' + ' => from: ' + STATIC_SERVER_PATH +
+    '\n' + ' => to: http://localhost:' + STATIC_SERVER_PORT +
     '\n' + ' => [ ctrl + c ] to quit'
   );
 });
